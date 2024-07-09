6095f1b048a9ed282cc2d91b76fb2822f764d213
Eduardo García <eduardo.garcia.ordonez@outlook.com>
2024-07-09 08:07:04 -0600

    se agrego otro ciclo

--- src/proyecto/de/git/pkg1/ProyectoDeGit1.java
+++ src/proyecto/de/git/pkg1/ProyectoDeGit1.java
@@ -7,8 +7,12 @@ public class ProyectoDeGit1 {
  
     public static void main(String[] args) {
         System.out.println("Mi primer proyecto");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 1; i < 10; i++) {
             System.out.println(i);
+        }
+        //esto es el ciclo de prueba 
+        for (int i = 1; i < 5; i++) {
+            System.out.println("Bachillerato");
         }
     }
     
